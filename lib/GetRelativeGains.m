9a1dd8883da4837e1aca0545b149e54c0ff0065a
Wei-Han Chen (Alex) <alex81527@gmail.com>
2023-06-05 15:28:08 -0400

    update

--- lib/GetRelativeGains.m
+++ lib/GetRelativeGains.m
@@ -2,9 +2,10 @@ function [RelativeGains] = GetRelativeGains(P_mk, PathToF, AoD, Params)
     ToFs = unique(PathToF(PathToF~=-1));
     Z = sum(PathToF == ToFs, 1) == size(PathToF, 1);
     if any(Z)
-        z = find(Z==1, 1);
+        z = find(Z==1); % find all beam indices with all paths present
         g_t = steervec(Params.PhasedArray.getElementPosition()/Params.Lambda, [AoD.'; zeros(1, length(AoD))]);
-        RelativeGains = P_mk(:, z)./P_mk(1, z)./(g_t'*Params.v(:, z));
+        RelativeGains = P_mk(:, z)./(g_t'*Params.v(:, z));
+        RelativeGains = mean(RelativeGains./RelativeGains(1,:), 2); % calibrate against the first peak
     else
         % Send One Extra Probe
         v = steervec(Params.PhasedArray.getElementPosition()/Params.Lambda, [AoD.'; zeros(1, length(AoD))]);
